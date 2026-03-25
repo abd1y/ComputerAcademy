ab28eb27147c9c78672b0e555c92f11496d974ea
Rossevelt <Abduillh160@gmail.com>
2026-03-26 00:25:28 +0300

    edit number 3

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,15 @@ import './css/style.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import './i18n';
-
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/ComputerAcademy">
+    <HashRouter>
     
     <App/>  
 
     
-    </BrowserRouter>
+    </HashRouter>
     
   </StrictMode>
 )
