ff59ca630f662bd5c28f16596254b1d7d7ebdd3d
Rossevelt <Abduillh160@gmail.com>
2026-03-26 04:00:44 +0300

    add translation

--- src/html/MyProfile/Bottom Design/MsgNoPostDoement.jsx
+++ src/html/MyProfile/Bottom Design/MsgNoPostDoement.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { useTranslation } from 'react-i18next'
 export default function MsgNoPostDoement() {
+  const {t}=useTranslation()
   return (
     <div className='MsgNoPostDoement'>
-    <i class="ri-calendar-close-fill"></i>  <p>This user does not have any published document.</p>
+    <i class="ri-calendar-close-fill"></i>  <p>{t("This user does not have any published document.")}</p>
     </div>
   )
 }
