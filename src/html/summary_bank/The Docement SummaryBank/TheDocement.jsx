baa3f7a92b957ea60fd1c2e0b4aebdac0605b7ec
Rossevelt <Abduillh160@gmail.com>
2026-03-26 04:26:51 +0300

    edit link summry bank

--- src/html/summary_bank/The Docement SummaryBank/TheDocement.jsx
+++ src/html/summary_bank/The Docement SummaryBank/TheDocement.jsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next'
 export default function TheDocement({itam}) {
   const {t,i18n}=useTranslation()
   return (
-    <a href={`http://127.0.0.1:8000${itam.File}`} download={itam.File_name} >
+    <a href={`https://abdullih123.pythonanywhere.com/${itam.File}`} download={itam.File_name} >
 
     
         <div  className='TheDocement' dir={i18n.language==="en"?"ltr":'rtl'}>
