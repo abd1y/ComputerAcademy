f895dadd4318768e335fc0d8d26cdfed989d846d
Rossevelt <Abduillh160@gmail.com>
2026-03-26 04:33:33 +0300

    data summary bank

--- src/dataLogo/dataLogo.jsx
+++ src/dataLogo/dataLogo.jsx
@@ -5,7 +5,7 @@ const dataLogo=[
     
    { imagdeportment:software,NameDeportment:"software Deportment",shortDeportment:'SW' },
    { imagdeportment:AI,NameDeportment:"AI Deportment",shortDeportment:'AI'},
-   { imagdeportment:cyber,NameDeportment:"Cybersecurity Deportment",shortDeportment:'CS'},
+   { imagdeportment:cyber,NameDeportment:"Cybersecurity Deportment",shortDeportment:'CY'},
    { imagdeportment:informationSystems ,NameDeportment:"Information Systems Deportment",shortDeportment:'IS'},
    { imagdeportment:computerNetwork ,NameDeportment:"Computer Network Deportment",shortDeportment:'CN'},
    { imagdeportment:MM,NameDeportment:"Multimedia Deportment",shortDeportment:'MM'},
