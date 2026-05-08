30a0583401eac6defce8940453d95914d52fb652
Rossevelt <Abduillh160@gmail.com>
2026-05-08 23:04:00 +0300

    loding effect to add comment

--- src/html/Section-Groups/Home-Groups/Post-Home-Groups/Coment Item/AddComent.jsx
+++ src/html/Section-Groups/Home-Groups/Post-Home-Groups/Coment Item/AddComent.jsx
@@ -5,11 +5,14 @@ import API from '../../../../Axios'
 export default function AddComent({commenthandler,post_id}) {
   const {t}=useTranslation()
   const [addComent,setaddComment]=useState("")
+  const [Loding,setloding]=useState(false)
    const {Group_code}=useParams()
   const token=JSON.parse(localStorage.getItem("Token"))
   const url=`Groups/add_comment/${post_id}/?Group_code=%23${Group_code}`
 const addComenthandler=()=>{
+
     if (!addComent.trim()) return
+    setloding(true)
 API.post(url,{content:addComent},{
     headers:{
         Authorization:`Token ${token}`
@@ -17,23 +20,33 @@ API.post(url,{content:addComent},{
 })
 .then(res=>{
   if(commenthandler) commenthandler(res.data)
+  
   setaddComment("")
+  setloding(false)
+  
 })
 .catch(
   err=>{
+setloding(false)
 
-    console.log(err.Respons)
   }
 )
+
 }
 
   return (
     <div className='Add-Coment'>
 <input type='Text' value={addComent} onChange={v=>setaddComment(v.target.value)} autoCapitalize='off' placeholder={t('Add Comment')}/>
-
-<div className='btm-comment-send' onClick={addComenthandler}>
+{
+  Loding?
+  (<div className='contener-loding-comment'>
+    <div className='loding-comment'></div>
+  </div>):
+  (<div className='btm-comment-send' onClick={addComenthandler}>
     <i class="ri-send-plane-2-fill"></i>
-</div>
+</div>)
+}
+
     </div>
   )
 }
