bebb7b2f2b4ad34f2e628480d25a07781a312d8f
Rossevelt <Abduillh160@gmail.com>
2026-05-08 22:23:31 +0300

    effict delete post

--- src/html/Section-Groups/Home-Groups/Post-Home-Groups/BtmEditAndDeletPostGroup.jsx
+++ src/html/Section-Groups/Home-Groups/Post-Home-Groups/BtmEditAndDeletPostGroup.jsx
@@ -1,9 +1,11 @@
 import {useState} from 'react'
-import { useTranslation } from 'react-i18next'
 import API from '../../../Axios'
+import { useTranslation } from 'react-i18next'
 import { useParams } from 'react-router-dom'
 export default function BtmEditAndDeletPostGroup({deletehandlerPost,data}) {
+       const {t}=useTranslation()
   const [List,setList]=useState(false)
+  const [loding,setLoding]=useState(false)
   const clickList=()=>{
     setList(!List)
   }
@@ -11,6 +13,7 @@ export default function BtmEditAndDeletPostGroup({deletehandlerPost,data}) {
   const Token=JSON.parse(localStorage.getItem('Token'))
   console.log("code",Token)
 const Deletehandler=()=>{
+  setLoding(true)
   API.delete(`Groups/delete_post/${data.post_id}/?Group_code=%23${Group_code}`,{
     headers:{
       Authorization:`Token ${Token}`
@@ -18,12 +21,13 @@ const Deletehandler=()=>{
   })
 .then(()=>{
   deletehandlerPost(data.post_id)
+  setLoding(false)
 })
   .catch(err=>{
-    alert("erorr")
+   setLoding(false)
   })
 }
-    const {t}=useTranslation()
+
     const data_save=JSON.parse(localStorage.getItem('data'))
     const id=data_save.id
   return (
@@ -32,7 +36,15 @@ const Deletehandler=()=>{
     <i class="ri-equalizer-line"></i>
   </div>
           <ul className='Tolls-Btm-post-Group' onClick={Deletehandler}  style={{display:List?"block":"none"}}>
-    <li>{t("Delete")}</li>
+{
+  loding?(
+    <div className='contener-loder-delete'>
+      <p>{t("Loding to delete")}</p>
+    
+  <div class="loaderDelete"></div></div>):(<li>{t("Delete")}</li>)
+}
+
+    
         </ul>
     </div>
   )
