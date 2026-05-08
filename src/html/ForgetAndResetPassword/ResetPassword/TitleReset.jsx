e7df05835443d7e7fce8e484072b8d650a90821f
Rossevelt <Abduillh160@gmail.com>
2026-05-08 21:07:18 +0300

    test

--- src/html/ForgetAndResetPassword/ResetPassword/TitleReset.jsx
+++ src/html/ForgetAndResetPassword/ResetPassword/TitleReset.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 export default function TitleReset() {
     const {t}=useTranslation()
+    const email=JSON.parse(localStorage.getItem("email"))
   return (
     <div className="TitleReset">
    
       <i class="ri-mail-unread-fill"></i>
       <p>
-        {t("Enter the code sent to ")}: example@student.uotechnology.edu.iq
+        {t("Enter the code sent to ")}: {email}
         <br /> <span>*</span>(
         {t("Check the Spam folder if you did not receive the code")})
       </p>
