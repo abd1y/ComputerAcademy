90a683528d4b9548aeb050f04cd8d3d96509fd68
Rossevelt <Abduillh160@gmail.com>
2026-06-10 00:12:34 +0300

    .c

--- src/html/summary_bank/filter and post/FilterAndPost.jsx
+++ src/html/summary_bank/filter and post/FilterAndPost.jsx
@@ -7,11 +7,11 @@ export default function FilterAndPost({SliderFilterhandler}) {
     <div className='FilterAndPost'>
       <div className='buttenSummry' id='buttrnPost'>
       <Link to={'DocementPost'}>
-        <button  className='buttenSummarypost' title={t("post summry")}> <i class="ri-pencil-fill"></i></button>
+        <button  className='buttenSummarypost' title={t("post summry")}><svg width={20} height={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path></svg></button>
       </Link>
       </div>
       <div id='buttrnFilter' className='buttenSummry'>   
-        <button  onClick={SliderFilterhandler} title={t("filter")}><i class="ri-equalizer-line"></i></button>
+        <button  onClick={SliderFilterhandler} title={t("filter")}><svg  width={20} height={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6.17071 18C6.58254 16.8348 7.69378 16 9 16C10.3062 16 11.4175 16.8348 11.8293 18H22V20H11.8293C11.4175 21.1652 10.3062 22 9 22C7.69378 22 6.58254 21.1652 6.17071 20H2V18H6.17071ZM12.1707 11C12.5825 9.83481 13.6938 9 15 9C16.3062 9 17.4175 9.83481 17.8293 11H22V13H17.8293C17.4175 14.1652 16.3062 15 15 15C13.6938 15 12.5825 14.1652 12.1707 13H2V11H12.1707ZM6.17071 4C6.58254 2.83481 7.69378 2 9 2C10.3062 2 11.4175 2.83481 11.8293 4H22V6H11.8293C11.4175 7.16519 10.3062 8 9 8C7.69378 8 6.58254 7.16519 6.17071 6H2V4H6.17071ZM9 6C9.55228 6 10 5.55228 10 5C10 4.44772 9.55228 4 9 4C8.44772 4 8 4.44772 8 5C8 5.55228 8.44772 6 9 6ZM15 13C15.5523 13 16 12.5523 16 12C16 11.4477 15.5523 11 15 11C14.4477 11 14 11.4477 14 12C14 12.5523 14.4477 13 15 13ZM9 20C9.55228 20 10 19.5523 10 19C10 18.4477 9.55228 18 9 18C8.44772 18 8 18.4477 8 19C8 19.5523 8.44772 20 9 20Z"></path></svg></button>
         </div>
     </div>
   )
