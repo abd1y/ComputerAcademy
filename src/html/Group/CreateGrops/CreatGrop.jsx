ea0a5e40055b75af738b18424ad41ff3fff9572b
Rossevelt <Abduillh160@gmail.com>
2026-06-09 22:55:12 +0300

    edit icon

--- src/html/Group/CreateGrops/CreatGrop.jsx
+++ src/html/Group/CreateGrops/CreatGrop.jsx
@@ -53,6 +53,7 @@ setloding(false)
 
 .catch(
   err=>{
+    console.log(err)
 setclickBtm(false)
 setloding(false)
 seterorr(true)
@@ -70,7 +71,7 @@ seterorr(true)
 
       
       <div className='Erorr-creat-groups' style={{display:erorr?"block":"none"}}>
-        <h1><i class="ri-error-warning-fill"></i> {t("The group name must not be empty or less than 3 letters long")}</h1>
+        <h1><svg className='Erorr-Messege' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="currentColor" ><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11 15V17H13V15H11ZM11 7V13H13V7H11Z"></path></svg> {t("The group name must not be empty or less than 3 letters long")}</h1>
       </div>
 <Notes/>
     </div>
