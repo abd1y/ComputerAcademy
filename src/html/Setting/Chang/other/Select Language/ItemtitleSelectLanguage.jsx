4d467b9ee15fe3baffa9181cbc040439c4947473
Rossevelt <Abduillh160@gmail.com>
2026-06-10 16:45:41 +0300

    edit icon setting

--- src/html/Setting/Chang/other/Select Language/ItemtitleSelectLanguage.jsx
+++ src/html/Setting/Chang/other/Select Language/ItemtitleSelectLanguage.jsx
@@ -4,9 +4,14 @@ export default function ItemtitleSelectLanguage({clickSlider,isOpne}) {
   const{t}=useTranslation()
       const {i18n}=useTranslation()
   return (
-     <div className='ItemtitleSelectLanguage' dir={i18n.language==='en'?'ltr':'rtl'} onClick={clickSlider}>
-            <h1>{t('Select Language')}</h1>
-           {isOpne?(<i class="ri-arrow-drop-up-line"></i>):(<i class="ri-arrow-drop-down-line"></i>)} 
-        </div>
-  )
+    <div
+      className="ItemtitleSelectLanguage"
+      dir={i18n.language === "en" ? "ltr" : "rtl"}
+      onClick={clickSlider}
+    >
+      <h1>{t("Select Language")}</h1>
+      {isOpne ?  (<svg className='arrow-drop' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 10L16 14H8L12 10Z"></path></svg>)
+        :(<svg className='arrow-drop' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 14L8 10H16L12 14Z"></path></svg>)}
+    </div>
+  );
 }
