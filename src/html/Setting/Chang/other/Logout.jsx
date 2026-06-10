2e9d1a7e38fcdfa720933d8eb6214bd63774cee0
Rossevelt <Abduillh160@gmail.com>
2026-06-10 16:42:25 +0300

    ...

--- src/html/Setting/Chang/other/Logout.jsx
+++ src/html/Setting/Chang/other/Logout.jsx
@@ -13,7 +13,7 @@ export default function Logout() {
   return (
    
        <div className='Logout itemOters'>
-      <button  onClick={Logouthandler} title={t("Logout")}><i class="ri-logout-box-r-line"></i></button>
+      <button  onClick={Logouthandler} title={t("Logout")}><svg className='Icon-itemOther' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M5 22C4.44772 22 4 21.5523 4 21V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V6H18V4H6V20H18V18H20V21C20 21.5523 19.5523 22 19 22H5ZM18 16V13H11V11H18V8L23 12L18 16Z"></path></svg></button>
     </div>
   
   )
--- src/html/Setting/Chang/other/Save.jsx
+++ src/html/Setting/Chang/other/Save.jsx
@@ -14,8 +14,8 @@ export default function Save({SettingAPI,loader}) {
        >
         
         {loader?
-        (<div className='Loder-save'><i class="ri-loader-3-line"></i></div>)
-          :(<i class="ri-save-fill"></i>)}</button>
+        (<div className='Loder-save'><svg className='Icon-itemOther' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3.05469 13H5.07065C5.55588 16.3923 8.47329 19 11.9998 19C15.5262 19 18.4436 16.3923 18.9289 13H20.9448C20.4474 17.5 16.6323 21 11.9998 21C7.36721 21 3.55213 17.5 3.05469 13ZM3.05469 11C3.55213 6.50005 7.36721 3 11.9998 3C16.6323 3 20.4474 6.50005 20.9448 11H18.9289C18.4436 7.60771 15.5262 5 11.9998 5C8.47329 5 5.55588 7.60771 5.07065 11H3.05469Z"></path></svg></div>)
+          :(<svg className='Icon-itemOther' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18 21V13H6V21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H17L21 7V20C21 20.5523 20.5523 21 20 21H18ZM16 21H8V15H16V21Z"></path></svg>)}</button>
     </div>
     </div>
   )
