fa558337400380690526df0450edafabbba6fcc9
Rossevelt <Abduillh160@gmail.com>
2026-06-10 14:38:19 +0300

    edit icon

--- src/html/summary_bank/filter and post/Post/Select Level/FrontSelectLevel.jsx
+++ src/html/summary_bank/filter and post/Post/Select Level/FrontSelectLevel.jsx
@@ -6,8 +6,8 @@ export default function FrontSelectLevel({ClickLevel,selectLevel}) {
    <div onClick={ClickLevel} className='FrontSelectLevel' dir={i18n.language==="en"?"ltr":"rtl"} >
         <h1>{t('Select Level')}</h1>
         {
-            selectLevel?(<i class="ri-arrow-drop-up-fill"></i>)
-        :(<i class="ri-arrow-drop-down-fill"></i>)
+            selectLevel?(<svg className='arrow-drop' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 10L16 14H8L12 10Z"></path></svg>)
+        :(<svg className='arrow-drop' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 14L8 10H16L12 14Z"></path></svg>)
         }
       </div>
   )
